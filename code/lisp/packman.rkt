29fb27a0f6ee6e7f7042071cc013e4f6887badd9
dmitry_b <dbochor@gmail.com>
2014-07-26 04:02:17 +0300

    Lot's of bug fixes in pakman lisp algorithm. But still has some bugs.

--- code/lisp/packman.rkt
+++ code/lisp/packman.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(define nil '())
+
 ; returns n-th element of the list
 (define (list-ref items n)
    (if (= n 0)
@@ -7,8 +9,8 @@
       (list-ref (cdr items) (- n 1))))
 
 ;returns element at the specified location of the matrix
-(define (matrix-ref matrix location)
-  (list-ref (list-ref matrix (car location)) (cdr location)))
+(define (matrix-ref matrix point)
+  (list-ref (list-ref matrix (cdr point)) (car point)))
 
 ;appends two lists
 (define (append list1 list2)
@@ -32,54 +34,82 @@
 
 ;maps and then accumulates with append given items
 (define (flatmap proc items)
-   (accumulate append nil (map proc seq)))
+   (accumulate append nil (map proc items)))
+
 
 
 ;maximum depth for depth search
 (define max-depth 3)
 
-(define (next-location world-map location move)
-  ((let new-location (cond ((= move 0) (cons (car location) (- (cdr location) 1)))
-         ((= move 1) (cons (- (car location) 1) (cdr location)))
-         ((= move 2) (cons (car location) (+ (cdr location) 1)))
-         ((= move 3) (cons (+ (car location) 1) (cdr location)))))
-   
-   (if (= (matrix-ref(world-map, location)) 0) location next-location)))
-   
+(define (next-move world-map point direction)
+  ((lambda (new-point) (if (= (matrix-ref world-map new-point) 0) 
+                           (list (make-move direction point)) (list (make-move direction new-point))))
+      (cond ((= direction 0) (cons (car point) (- (cdr point) 1)))
+         ((= direction 1) (cons (- (car point) 1) (cdr point)))
+         ((= direction 2) (cons (car point) (+ (cdr point) 1)))
+         ((= direction 3) (cons (+ (car point) 1) (cdr point))))
+   ))
+
+
+(define (list-possible-moves world-map point) 
+  (flatmap (lambda (direction) (next-move world-map point direction))
+     (list 0 1 2 3)))
 
 ;pills, power pills, fruits
-(define (tick-man world-map location)
-  ((let (content matrix-ref(world-map, location)))
-  (cond ((= content 1) 0)
+(define (tick-man world-map point)
+  ((lambda (content) 
+     (cond ((= content 1) 0)
         ((= content 2) 10)
         ((= content 3) 50)
         ((= content 4) 100)
-        else 0)))
+        (else 0)))
+   (matrix-ref world-map point)))
 
 
-(define (list-next-locations world-map location) 
-  (flatmap (lambda (move) (next-location world-map location move))
-     (list 0 1 2 3)))
+(define (evaluate-moves world-map moves-list depth)
+  (map (lambda (move) (tick-world world-map move (+ depth 1))) moves-list))
 
 
-(define (tick-world world-map location depth)
-   (
-   ;tick-actions
-   (let (score (tick-man world-map location)))
-   ;(+score) tick-ghosts
+(define (make-path-node score move)
+  (cons score move))
 
-   (if (> depth max-depth)
-      score   
-      (+ score 
-         (accumulate max 0 
-                     (map (lambda (loc) (tick-world world-map loc (+ depth 1)))
-                          (list-next-locations world-map location)))))))
+(define (path-score path)
+  (car (list-ref path 0)))
+
+(define (make-move direction point)
+  (cons direction point))
+
+(define (move-point move) 
+  (cdr move))
+
+(define (append-path score move path)
+   (cons (make-path-node (+ score (path-score path)) move) path))
+
+(define (max-path p1 p2)
+  (if (null? p2) p1 (if (> (path-score p1) (path-score p2)) p1 p2)))
+
+(define (tick-world world-map move depth)
+   (if (> depth max-depth) 
+       (list (make-path-node 0 move))
+       (append-path (tick-man world-map (move-point move)) 
+                    move
+          ;tick-actions
+          ;(+score) tick-ghosts          
+                    (accumulate max-path nil 
+                       (evaluate-moves world-map (list-possible-moves world-map (move-point move)) depth))
+        )))
 
 
 ;AI step for packman
-;TODO: The function tick-world is broken now. It calculates top score but 
-; it also should return optimal move for the top score. To fix!!!!
 (define (step-map ai-state world-state)
-  (tick-world (list-ref world-state 0) (matrix-ref world-state 1 1) 0)
+  (tick-world (list-ref world-state 0) (make-move 0 (matrix-ref world-state 1 1)) 0)
   (list 0 1))
 
+(define test-map 
+  (list (list 0 0 0 0 0)
+        (list 0 2 2 1 0)
+        (list 0 0 0 0 0)))
+
+;(list-possible-moves test-map (cons 3 1))
+
+(tick-world test-map (make-move 0 (cons 3 1)) 0)
